6d472714e3e8c1adf58cb93f167ad140e864f0c9
zhouhao <zh175369@alibaba-inc.com>
2018-11-06 16:47:45 +0800

    run springBoot application

--- src/test/java/com/zh/study/StudyApplicationTests.java
+++ src/test/java/com/zh/study/StudyApplicationTests.java
@@ -1,16 +1,13 @@
 package com.zh.study;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootApplication
 public class StudyApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplicationTests.class, args);
 	}
 
 }
