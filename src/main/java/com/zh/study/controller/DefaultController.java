822088ba1851c56f1400b59ab33d93a70271936d
zhouhao <zh175369@alibaba-inc.com>
2018-12-03 14:23:32 +0800

    add reademe

--- src/main/java/com/zh/study/controller/DefaultController.java
+++ src/main/java/com/zh/study/controller/DefaultController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.Contended;
 
 /**
  * @Author yexun
